63b3838a96e63d3bb6acf014687cd114e5b4a7e5
Jehu Zepeda-Silva <jehuzepeda@yahoo.com>
2025-01-08 02:14:38 -0800

    cleaned up sanitation function

--- src/Section.jsx
+++ src/Section.jsx
@@ -11,9 +11,11 @@ import { useEditable } from './handleEditable';
 
 function sanitizeInput(input) {
   console.log(input);
-  let sanitizedString = input.replace(/&nbsp;/g, ' ') // Replace non-breaking spaces
-                                   .replace(/<br\s*\/?>/g, '\n')
-                                   .replace(/&amp;/g, '&'); 
+  let sanitizedString = (
+      input.replace(/&nbsp;/g, ' ') // Replace non-breaking spaces
+      .replace(/<br\s*\/?>/g, '\n')
+      .replace(/&amp;/g, '&')
+    ); 
 
   sanitizedString = sanitizedString.trim();
   console.log(sanitizedString)
